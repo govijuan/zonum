da0b4d2c7add07cf6d4dbb44e9802983ec106a1d
Juan Carlos Coral <juan.carlos@planet108.de>
2015-11-05 10:08:01 -0200

    Added centered text in the slides, and solved the issue of bad resizing at the on-window-load loop in javascript

--- js/proyectos.js
+++ js/proyectos.js
@@ -1,5 +1,6 @@
 
 var sliderObj = new Object();
+var sliderTextWrapperWidth = 0;
 function createSlickSlider(){
 	jQuery('.project-holder').slick({
 				
@@ -39,15 +40,20 @@ function setprojectsHolderWidth(){
 }
 
 function setSlidedimentions(){
-	var oneThirdWidth = Math.round(windowWidth/3);
+	var oneThirdWidth = Math.round((windowWidth - 120) / 3);
 	jQuery('.slick-slide').css('width', oneThirdWidth +'px');
 	var photoWrapperWidth = jQuery('.photo-wrapper').width();
 	var photoWrapperheight = photoWrapperWidth * 0.6494;
 	var sliderPhotoWidth = Math.round(photoWrapperWidth * 1.394);
 	jQuery('.photo-wrapper').css('height', photoWrapperheight + 'px');
-	var sliderTextWrapperWidth = ((windowWidth - 120) / 3) -10;
+	sliderTextWrapperWidth = ((windowWidth - 120) / 3) -10;
 	jQuery('.photo-wrapper img').css('width', sliderPhotoWidth + 'px');
 	jQuery('.text-wrapper, .project-over').css('width', sliderTextWrapperWidth + 'px');
+	var sliderTitleHeight = jQuery('.text-wrapper h2').height();
+	var sliderParagraphHeight = jQuery('.text-wrapper p').height();
+	var sliderTextTopDistance = (photoWrapperheight - (sliderTitleHeight + sliderParagraphHeight))/ 2;
+	jQuery('.text-wrapper h2').css('margin-top', sliderTextTopDistance + 'px');
+	//console.log(sliderParagraphHeight);
 }
 
 /*function slideHoverBehavior(){
@@ -60,12 +66,15 @@ var SliderObj = new Object();
 var windowWidth = 0;
 var projectHolderHeight = 0;
 
-jQuery(document).ready(function($){
+jQuery(document).ready(function(){
 	windowWidth = jQuery(window).width();
-	
+    createSlickSlider();
     setprojectsHolderWidth();
-   	createSlickSlider();
-	setSlidedimentions();
+    setSlidedimentions();
+    
+   	
+   	
+	
 	//slideHoverBehavior();
 	jQuery('.project-over').mouseenter(function(){jQuery(this).siblings('.text-wrapper').fadeOut( 200 )});
 	jQuery('.project-over').mouseleave(function(){jQuery(this).siblings('.text-wrapper').fadeIn( 200 )});
